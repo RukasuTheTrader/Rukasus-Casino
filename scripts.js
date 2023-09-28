f03896743e0237d2d1b197316131c2d41275d3c9
RukasuTheTrader <145609815+RukasuTheTrader@users.noreply.github.com>
2023-09-29 00:58:36 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,75 +1,75 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-let bet = 10; // Der Einsatz des Spielers pro Spiel
+let bet = 10; // The player's bet per game
 let betSlider = document.getElementById('betSlider');
 let betDisplay = document.getElementById('betDisplay');
 
 betSlider.addEventListener('input', function() {
     let betValue = parseInt(betSlider.value, 10);
     if (betValue > jackBalance) {
-        alert('Ihr Einsatz kann nicht höher sein als Ihr Jack Balance!');
+        alert('Your bet cannot be higher than your Jack balance!');
         betSlider.value = bet;
     } else {
         bet = betValue;
-        betDisplay.textContent = 'Einsatz: ' + bet;
+        betDisplay.textContent = 'Bet: ' + bet;
     }
 });
 
 let playerScore = 0;
 let hostScore = 0;
 let jackBalance = 1000;
-let bet = 10; // Der Einsatz des Spielers pro Spiel
+let bet = 10; // The player's bet per game
 
-// Initialisierung des Spiels
+// Game Initialization
 function initGame() {
-    // Einsatz abziehen
+    // Deducting the bet
     jackBalance -= bet;
     document.getElementById('jackBalance').textContent = "Jack: " + jackBalance;
     playerScore = 0;
     hostScore = 0;
     betSlider.value = bet;
-    betDisplay.textContent = 'Einsatz: ' + bet;
-    document.getElementById('playerScore').textContent = "Spieler: " + playerScore;
+    betDisplay.textContent = 'Bet: ' + bet;
+    document.getElementById('playerScore').textContent = "Player: " + playerScore;
     document.getElementById('hostScore').textContent = "Host: " + hostScore;
     document.getElementById('jackBalance').textContent = "Jack: " + jackBalance;
     document.getElementById('rollBtn').disabled = false;
     document.getElementById('holdBtn').disabled = false;
 }
 
-// Wenn der Spieler würfelt
+// When the player rolls
 document.getElementById('rollBtn').addEventListener('click', function() {
     let roll = Math.floor(Math.random() * 100) + 1;
-    // Überprüfen der Sonderregel
+    // Checking the special rule
     if (roll <= 4 && playerScore === 0) {
-        endGame(false); // Host gewinnt sofort
+        endGame(false); // Host wins immediately
         return;
     }
     playerScore += roll;
-    document.getElementById('playerScore').textContent = "Spieler: " + playerScore;
+    document.getElementById('playerScore').textContent = "Player: " + playerScore;
     if (playerScore > 100) {
-        endGame(false); // Spieler hat verloren
+        endGame(false); // Player has lost
     }
 });
 
-// Wenn der Spieler hält
+// When the player holds
 document.getElementById('holdBtn').addEventListener('click', function() {
     document.getElementById('rollBtn').disabled = true;
     document.getElementById('holdBtn').disabled = true;
     while (hostScore < playerScore && hostScore <= 100) {
         let roll = Math.floor(Math.random() * 100) + 1;
         if (roll <= 4 && hostScore === 0) {
-            endGame(true); // Spieler gewinnt sofort
+            endGame(true); // Player wins immediately
             return;
         }
         hostScore += roll;
         document.getElementById('hostScore').textContent = "Host: " + hostScore;
     }
     if (hostScore > 100 || hostScore < playerScore) {
-        endGame(true); // Spieler hat gewonnen
+        endGame(true); // Player has won
     } else if (hostScore === playerScore) {
-        endGame('draw'); // Unentschieden
+        endGame('draw'); // Draw
     } else {
-        endGame(false); // Spieler hat verloren
+        endGame(false); // Player has lost
     }
 });
 
@@ -77,20 +77,18 @@ function endGame(result) {
     document.getElementById('rollBtn').disabled = true;
     document.getElementById('holdBtn').disabled = true;
     if (result === true) {
-    jackBalance += bet * 2; // Richtig: Gibt das Doppelte des Einsatzes zurück
-    }
+        jackBalance += bet * 2; // Correct: Returns double the bet
     } else if (result === 'draw') {
-        jackBalance += bet; // Einsatz wurde bereits abgezogen, also nur + bet
-    }
+        jackBalance += bet; // Bet has already been deducted, so only + bet
     } else {
         jackBalance -= bet;
     }
     document.getElementById('jackBalance').textContent = "Jack: " + jackBalance;
-    // Nach einer kurzen Pause, Spiel zurücksetzen
+    // Resetting the game after a short pause
     setTimeout(initGame, 3000);
 }
 
-// Spiel starten
+// Starting the game
 initGame();
 
 });
